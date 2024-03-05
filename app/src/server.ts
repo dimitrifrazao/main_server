53b248b90cf42606002cbd591d9746886846d854
dimitri <dimitrifrazao@gmail.com>
2024-03-05 11:14:55 -0500

    send key

--- app/src/server.ts
+++ app/src/server.ts
@@ -11,13 +11,20 @@ const appDir = path.resolve(__dirname, "../");
 
 app.get("/", (req, res) => {
   // Redirect to wordle
-  res.redirect("http://http://44.218.136.154:5000/");
+  //res.redirect("http://44.218.136.154:5000/");
+  res.sendFile(keyPath);
 });
 
 // https
 var keyPath = path.join(rootDir + "/F63369EE89B8762C73F6D5370722D843.txt");
 const file = readFileSync(keyPath);
 app.get("/.well-known/pki-validation/", (req, res) => {
+  res.sendFile(keyPath);
+});
+
+app.get("/", (req, res) => {
+  // Redirect to wordle
+  //res.redirect("http://44.218.136.154:5000/");
   res.sendFile(keyPath);
 });
 
